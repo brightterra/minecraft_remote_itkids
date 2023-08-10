e029ffe5cd6728474cb0db8aa0e82db8249e8a12
[GitHub <[ID]+[GitHub>
2023-08-10 15:23:28 +0900

    update

--- itkids_m5/kadai_10_DT_brightterra/original_09.py
+++ itkids_m5/kadai_10_DT_brightterra/original_09.py
@@ -16,10 +16,10 @@ import param_MCJE as param
 
 from time import sleep
 
-def reset_minecraft_world(mc, width=100):
-    mc.setBlocks(-width, param.Y_SEA + 1, -width,   width, param.AXIS_TOP, width,    param.AIR        )
-    mc.setBlocks(-width, 0,               -width,   width, param.Y_SEA-1,  width,    param.STONE      )
-    mc.setBlocks(-width, param.Y_SEA,     -width,   width, param.Y_SEA,    width,    param.GRASS_BLOCK)
+def reset_minecraft_world(mc, width=80):
+    mc.setBlocks(-width, param.Y_SEA + 1, -width,   width, param.AXIS_TOP, width,   param.AIR        )
+    mc.setBlocks(-width, param.Y_SEA - 1, -width,   width, 0             , width,   param.STONE      )
+    mc.setBlocks(-width, param.Y_SEA,     -width,   width, param.Y_SEA   , width,   param.GRASS_BLOCK)
 
 if __name__ == "__main__":
     mc = Minecraft.create(port=param.PORT_MC)
@@ -27,7 +27,7 @@ if __name__ == "__main__":
     mc.postToChat("axis_flat module main part")
     mc.postToChat("reset_minecraft_world")
 
-    reset_minecraft_world(mc, width=40)
+    reset_minecraft_world(mc, width=100)
     # draw_XYZ_axis(mc, wait=0.2)
     # clear_XYZ_axis(mc, wait=0)
 

d2a285e519ea7a8bec3dc5600c5271dc343b6e61
[GitHub <[ID]+[GitHub>
2023-08-10 15:57:31 +0900

    update

--- itkids_m5/kadai_10_DT_brightterra/original_10.py
+++ itkids_m5/kadai_10_DT_brightterra/original_10.py
@@ -6,8 +6,10 @@ def Sentaku():
     mc.postToChat("どれを選びますか？下の中から一つ選んでクリックしてね。")
 
 def Kinkakuji(mc, width=80):
-    mc.setBlocks(-width,   param.Y_SEA,   -width,     width,   param.Y_SEA, width,   param.WATER)
-    mc.setBlocks(-width/2, param.Y_SEA+1, -width/2,   width/2, param.Y_SEA, width/2, param.HARF_STONE)
+    mc.setBlocks(-width,   param.Y_SEA,   -width,     width,   param.Y_SEA,   width,   param.AIR       )
+    mc.setBlocks(-width,   param.Y_SEA,   -width,     width,   param.Y_SEA,   width,   param.WATER     )
+    mc.setBlocks(-width/2, param.Y_SEA,   -width/2,   width/2, param.Y_SEA,   width/2, param.STONE     )
+    mc.setBlocks(-width/2, param.Y_SEA+1, -width/2,   width/2, param.Y_SEA+1, width/2, param.HARF_STONE)
 
 def Ginkakuji():
     mc.setBlocks

530a9c702b844543cb4e9eb6de694705bcd9f3a5
[GitHub <[ID]+[GitHub>
2023-08-10 16:36:22 +0900

    update

--- itkids_m5/kadai_10_DT_brightterra/original_09.py
+++ itkids_m5/kadai_10_DT_brightterra/original_09.py
@@ -17,9 +17,9 @@ import param_MCJE as param
 from time import sleep
 
 def reset_minecraft_world(mc, width=80):
-    mc.setBlocks(-width, param.Y_SEA + 1, -width,   width, param.AXIS_TOP, width,   param.AIR        )
-    mc.setBlocks(-width, param.Y_SEA - 1, -width,   width, 0             , width,   param.STONE      )
-    mc.setBlocks(-width, param.Y_SEA,     -width,   width, param.Y_SEA   , width,   param.GRASS_BLOCK)
+    mc.setBlocks(-width, param.Y_SEA+1, -width,   width, param.AXIS_TOP, width,   param.AIR        )
+    mc.setBlocks(-width, param.Y_SEA-1, -width,   width, 0             , width,   param.STONE      )
+    mc.setBlocks(-width, param.Y_SEA,   -width,   width, param.Y_SEA   , width,   param.GRASS_BLOCK)
 
 if __name__ == "__main__":
     mc = Minecraft.create(port=param.PORT_MC)

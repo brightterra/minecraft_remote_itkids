920c454f857c1c6650a9e555f0c926f0425f899d
[GitHub <[ID]+[GitHub>
2023-08-10 16:58:48 +0900

    update

--- itkids_m5/kadai_10_DT_brightterra/original_10.py
+++ itkids_m5/kadai_10_DT_brightterra/original_10.py
@@ -5,7 +5,7 @@ from time import sleep
 def Sentaku():
     mc.postToChat("どれを選びますか？下の中から一つ選んでクリックしてね。")
 
-def Kinkakuji(mc, width=40):
+def Kinkakuji(mc, width=20):
     mc.setBlocks(-width,   param.Y_SEA+1,   -width,      width,    param.Y_SEA+1, width,    param.HARF_STONE)
     mc.setBlocks(-width+2, param.Y_SEA+1,   -width+2,    width-2,  param.Y_SEA+1, width-2,  param.STONE     )
     mc.setBlocks(-width+2, param.Y_SEA+2,   -width+2,    width-2,  param.Y_SEA+2, width-2,  param.FENCE     )
@@ -14,4 +14,4 @@ def Kinkakuji(mc, width=40):
 if __name__ == "__main__":
     mc = Minecraft.create(port=param.PORT_MC)
     
-    Kinkakuji(mc, width=40)
+    Kinkakuji(mc, width=20)

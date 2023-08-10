de4ae4f0189716c602477d37425ba04dbc2c1d0a
[GitHub <[ID]+[GitHub>
2023-08-10 15:59:21 +0900

    update

--- itkids_m5/kadai_10_DT_brightterra/original_10.py
+++ itkids_m5/kadai_10_DT_brightterra/original_10.py
@@ -5,11 +5,11 @@ from time import sleep
 def Sentaku():
     mc.postToChat("どれを選びますか？下の中から一つ選んでクリックしてね。")
 
-def Kinkakuji(mc, width=80):
-    mc.setBlocks(-width,   param.Y_SEA,   -width,     width,   param.Y_SEA,   width,   param.AIR       )
-    mc.setBlocks(-width,   param.Y_SEA,   -width,     width,   param.Y_SEA,   width,   param.WATER     )
-    mc.setBlocks(-width/2, param.Y_SEA,   -width/2,   width/2, param.Y_SEA,   width/2, param.STONE     )
-    mc.setBlocks(-width/2, param.Y_SEA+1, -width/2,   width/2, param.Y_SEA+1, width/2, param.HARF_STONE)
+def Kinkakuji(mc, width=80, width2=40):
+    mc.setBlocks(-width,  param.Y_SEA,   -width,    width,  param.Y_SEA,   width,  param.AIR       )
+    mc.setBlocks(-width,  param.Y_SEA,   -width,    width,  param.Y_SEA,   width,  param.WATER     )
+    mc.setBlocks(-width2, param.Y_SEA,   -width2,   width2, param.Y_SEA,   width2, param.STONE     )
+    mc.setBlocks(-width2, param.Y_SEA+1, -width2,   width2, param.Y_SEA+1, width2, param.HARF_STONE)
 
 def Ginkakuji():
     mc.setBlocks

f8a75aacdf5f05a61b16925015743756acd6ead2
[GitHub <[ID]+[GitHub>
2023-08-11 14:26:13 +0900

    update

--- itkids_m5/kadai_10_DT_brightterra/param_MCJE.py
+++ itkids_m5/kadai_10_DT_brightterra/param_MCJE.py
@@ -23,7 +23,7 @@ PORT_MC = 14712
 # block IDs  You can find IDs here: https://minecraft-ids.grahamedgecombe.com/
 AIR = "air"
 FENCE = "spruce_fence"
-DARK = "spruce_wood_plank"
+DARK = "spruce_planks"
 STONE = "stone"
 GRASS_BLOCK = "grass_block"
 GOLD_BLOCK = "gold_block"

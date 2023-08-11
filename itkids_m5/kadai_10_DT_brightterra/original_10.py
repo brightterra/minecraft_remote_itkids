b1d2b28f3b435e109a5537432d6373c1bbb731f1
[GitHub <[ID]+[GitHub>
2023-08-11 14:21:50 +0900

    update

--- itkids_m5/kadai_10_DT_brightterra/original_10.py
+++ itkids_m5/kadai_10_DT_brightterra/original_10.py
@@ -7,7 +7,7 @@ def Sentaku():
 
 def Kinkakuji(mc, width=20):
     mc.setBlocks(-width,   param.Y_SEA+1, -width,    width,    param.Y_SEA+1, width,    param.HARF_STONE)
-    mc.setBlocks(-width+1, param.Y_SEA+1, -width+1,  width-1,  param.Y_SEA+1, width-1,  param.STONE     )
+    mc.setBlocks(-width+1, param.Y_SEA+1, -width+1,  width-1,  param.Y_SEA+1, width-1,  param.DARK      )
     mc.setBlocks(-width+1, param.Y_SEA+2, -width+1,  width-1,  param.Y_SEA+2, width-1,  param.FENCE     )
     mc.setBlocks(-width+2, param.Y_SEA+2, -width+2,  width-2,  param.Y_SEA+2, width-2,  param.AIR       )
     mc.setBlocks(-width+1, param.Y_SEA+2, -width+4,  -width+1, param.Y_SEA+2, width,    param.AIR       )

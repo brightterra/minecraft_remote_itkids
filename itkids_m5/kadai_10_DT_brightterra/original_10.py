918042b99f9121bb8b851fca4f2f978d910d09d6
[GitHub <[ID]+[GitHub>
2023-08-11 13:52:54 +0900

    update

--- itkids_m5/kadai_10_DT_brightterra/original_10.py
+++ itkids_m5/kadai_10_DT_brightterra/original_10.py
@@ -6,10 +6,10 @@ def Sentaku():
     mc.postToChat("どれを選びますか？下の中から一つ選んでクリックしてね。")
 
 def Kinkakuji(mc, width=20):
-    mc.setBlocks(-width,   param.Y_SEA+1,   -width,      width,    param.Y_SEA+1, width,    param.HARF_STONE)
-    mc.setBlocks(-width+2, param.Y_SEA+1,   -width+2,    width-2,  param.Y_SEA+1, width-2,  param.STONE     )
-    mc.setBlocks(-width+2, param.Y_SEA+2,   -width+2,    width-2,  param.Y_SEA+2, width-2,  param.FENCE     )
-    mc.setBlocks(-width+3, param.Y_SEA+2,   -width+3,    width-3,  param.Y_SEA+2, width-3,  param.AIR       )
+    mc.setBlocks(-width,   param.Y_SEA+1,   -width,      width,     param.Y_SEA+1, width,    param.HARF_STONE)
+    mc.setBlocks(-width+1, param.Y_SEA+1,   -width+1,    width-1,   param.Y_SEA+1, width-1,  param.STONE     )
+    mc.setBlocks(-width+2, param.Y_SEA+2,   -width+2,    -width+3,  param.Y_SEA+2, width-2,  param.FENCE     )
+    mc.setBlocks(-width+3, param.Y_SEA+2,   -width+3,    -width+3,  param.Y_SEA+2, width-3,  param.AIR       )
 
 if __name__ == "__main__":
     mc = Minecraft.create(port=param.PORT_MC)
